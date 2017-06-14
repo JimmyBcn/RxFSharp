5f0734abd163faa32ec82b7b35b1ec924a10fa59
jvinyes <vinyes@outlook.com>
2017-06-14 08:00:41 +0200

    Concurrency project created

---
+++ RxFSharp.Concurrency/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace RxFSharp.Concurrency.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("RxFSharp.Concurrency")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Microsoft")>]
+[<assembly: AssemblyProduct("RxFSharp.Concurrency")>]
+[<assembly: AssemblyCopyright("Copyright © Microsoft 2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("0ba0f6ff-a750-4cc1-b910-4cb7fdaf7bc3")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
